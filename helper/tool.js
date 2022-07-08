05141a1733507f2477a8c1f7f0c319876c54e22d
theajack <1506282385@qq.com>
2022-07-08 08:55:41 +0800

    feat: 1

--- helper/tool.js
+++ helper/tool.js
@@ -4,6 +4,8 @@
  * @Description: Coding something
  */
 const childProcess = require('child_process');
+const path = require('path');
+const fs = require('fs');
 
 async function exec (cmd) {
     return new Promise(resolve => {
@@ -20,8 +22,18 @@ async function exec (cmd) {
         });
     });
 }
-
+function resolvePath (filePath) {
+    if (filePath[0] === '@' || filePath[0] === '/') {
+        filePath = '../' + filePath.substring(1);
+    }
+    return path.resolve(__dirname, filePath);
+}
+function writeJsonFile (filePath, json) {
+    fs.writeFileSync(resolvePath(filePath), JSON.stringify(json, null, 4), {encoding: 'utf-8'});
+}
 
 module.exports = {
     exec,
+    writeJsonFile,
+    resolvePath,
 };
