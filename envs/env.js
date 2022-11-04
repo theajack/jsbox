c576148169bbb952e5f2ccb0c2ea148f86276a60
theajack <1506282385@qq.com>
2022-11-04 14:46:24 +0000

    Deploying to gh-pages from @ theajack/jsbox@4ecf64c2af85a9ebe71396d9d22decb097314438 🚀

---
+++ envs/env.js
@@ -0,0 +1,106 @@
+"use strict";
+
+window.jsbox_envs = {
+  'element-ui': {
+    lang: 'html',
+    code: /* html*/"<div id='app'>\n<el-form ref=\"form\" :model=\"form\" label-width=\"80px\">\n    <el-form-item label=\"\u6D3B\u52A8\u540D\u79F0\">\n    <el-input v-model=\"form.name\"></el-input>\n    </el-form-item>\n    <el-form-item label=\"\u6D3B\u52A8\u533A\u57DF\">\n    <el-select v-model=\"form.region\" placeholder=\"\u8BF7\u9009\u62E9\u6D3B\u52A8\u533A\u57DF\">\n        <el-option label=\"\u533A\u57DF\u4E00\" value=\"shanghai\"></el-option>\n        <el-option label=\"\u533A\u57DF\u4E8C\" value=\"beijing\"></el-option>\n    </el-select>\n    </el-form-item>\n    <el-form-item label=\"\u6D3B\u52A8\u65F6\u95F4\">\n    <el-col :span=\"11\">\n        <el-date-picker type=\"date\" placeholder=\"\u9009\u62E9\u65E5\u671F\" v-model=\"form.date1\" style=\"width: 100%;\"></el-date-picker>\n    </el-col>\n    <el-col class=\"line\" :span=\"2\">-</el-col>\n    <el-col :span=\"11\">\n        <el-time-picker placeholder=\"\u9009\u62E9\u65F6\u95F4\" v-model=\"form.date2\" style=\"width: 100%;\"></el-time-picker>\n    </el-col>\n    </el-form-item>\n    <el-form-item label=\"\u5373\u65F6\u914D\u9001\">\n    <el-switch v-model=\"form.delivery\"></el-switch>\n    </el-form-item>\n    <el-form-item label=\"\u6D3B\u52A8\u6027\u8D28\">\n    <el-checkbox-group v-model=\"form.type\">\n        <el-checkbox label=\"\u7F8E\u98DF/\u9910\u5385\u7EBF\u4E0A\u6D3B\u52A8\" name=\"type\"></el-checkbox>\n        <el-checkbox label=\"\u5730\u63A8\u6D3B\u52A8\" name=\"type\"></el-checkbox>\n        <el-checkbox label=\"\u7EBF\u4E0B\u4E3B\u9898\u6D3B\u52A8\" name=\"type\"></el-checkbox>\n        <el-checkbox label=\"\u5355\u7EAF\u54C1\u724C\u66DD\u5149\" name=\"type\"></el-checkbox>\n    </el-checkbox-group>\n    </el-form-item>\n    <el-form-item label=\"\u7279\u6B8A\u8D44\u6E90\">\n    <el-radio-group v-model=\"form.resource\">\n        <el-radio label=\"\u7EBF\u4E0A\u54C1\u724C\u5546\u8D5E\u52A9\"></el-radio>\n        <el-radio label=\"\u7EBF\u4E0B\u573A\u5730\u514D\u8D39\"></el-radio>\n    </el-radio-group>\n    </el-form-item>\n    <el-form-item label=\"\u6D3B\u52A8\u5F62\u5F0F\">\n    <el-input type=\"textarea\" v-model=\"form.desc\"></el-input>\n    </el-form-item>\n    <el-form-item>\n    <el-button type=\"primary\" @click=\"onSubmit\">\u7ACB\u5373\u521B\u5EFA</el-button>\n    <el-button>\u53D6\u6D88</el-button>\n    </el-form-item>\n</el-form>\n</div>\n<script>\nvar app = new Vue({\n    el: '#app',\n    data() {\n        return {\n            form: {\n            name: '',\n            region: '',\n            date1: '',\n            date2: '',\n            delivery: false,\n            type: [],\n            resource: '',\n            desc: ''\n            }\n        }\n    },\n    methods: {\n        onSubmit() {\n            console.log('submit!', this.form);\n        }\n    }\n})\n</script>",
+    deps: ['element-ui']
+  },
+  'cnchar': {
+    code: /* javascript*/"\nlog(cnchar.version);\nlog('\u4E2D\u534E\u4EBA\u6C11\u5171\u548C\u56FD'.spell());\nlog('\u4E2D\u534E\u4EBA\u6C11\u5171\u548C\u56FD'.stroke('array'));\n",
+    deps: ['cnchar-all']
+  },
+  'alins': {
+    wrapCode: true,
+    needUI: true,
+    hideLog: true,
+    useDefaultUI: true,
+    clearWhenReRun: true,
+    code: /* javascript */"const { button, comp, click, $, mount } = Alins;\n\nfunction Count () {\n    const count = $(0);\n    return button(\n        click(() => {count.value++;}),\n        $`Count is ${count}`\n    );\n}\n\ncomp(Count).mount('#jx-app');",
+    deps: ['alins']
+  },
+  'alins-style': {
+    wrapCode: true,
+    needUI: true,
+    hideLog: true,
+    useDefaultUI: true,
+    clearWhenReRun: true,
+    code: /* javascript */"const {\n    div, $ , button, hover, click, input, cls, css, style, comp\n} = AlinsStyle;\nfunction Style () {\n    const num = $(30);\n    const active = $(false);\n\n    css('.main')(\n        style({\n            color: '#888',\n            marginLeft: num,\n        }),\n        ['&.active', style.fontSize(num)],\n        ['.child', style.marginTop(num)]\n    ).mount();\n\n    return div('parent.main',\n        cls({active}),\n        hover('color: #f44'),\n        input.model(num, 'number'),\n        button('toggle active', click(() => active.value = !active.value)),\n        div('child.child'),\n    );\n}\n\ncomp(Style).mount('#jx-app');",
+    deps: ['alins-style']
+  },
+  'vue': {
+    code: /* html*/"<div id=\"app\">\n    <div>{{ message }}</div>\n    <button @click='add'>btn</button>\n</div>\n<script>\n    var app = new Vue({\n        el: '#app',\n        data () {\n            return {\n                message: 'Hello Vue!'\n            }\n        },\n        methods: {\n            add(){\n                this.message += '!';\n            }\n        }\n    })\n</script>",
+    lang: 'html',
+    deps: ['vue']
+  },
+  'vue3': {
+    code: /* html*/"<div id=\"app\">{{ message }}</div>\n<script babel>\n    const { createApp } = Vue;\n    createApp({\n        data() {\n            return {\n                message: 'Hello Vue!'\n            }\n        }\n    }).mount('#app')\n</script>",
+    lang: 'html',
+    deps: ['vue@3']
+  },
+  'react': {
+    code: /* html*/"<div id=\"example\"></div>\n<script babel react>\n    ReactDOM.render(\n        <h1>Hello, world!</h1>,\n        document.getElementById('example')\n    );\n</script>",
+    lang: 'html',
+    deps: ['react-dom']
+  },
+  'echarts': {
+    code: /* html*/"<div id='echartsMain' style='height:300px'></div>\n<script>\nvar chartDom = document.getElementById('echartsMain');\nvar myChart = echarts.init(chartDom);\nvar option = {\n  xAxis: {\n    type: 'category',\n    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']\n  },\n  yAxis: {\n    type: 'value'\n  },\n  series: [\n    {\n      data: [150, 230, 224, 218, 135, 147, 260],\n      type: 'line'\n    }\n  ]\n};\nmyChart.setOption(option);\n</script>",
+    lang: 'html',
+    deps: ['echarts']
+  },
+  'angularjs': {
+    code: "<div ng-app=\"myApp\" ng-controller=\"myCtrl\">\n    \u540D\u5B57: <input ng-model=\"name\">\n    <h1>\u4F60\u8F93\u5165\u4E86: {{name}}</h1>\n</div>\n\n<script>\nvar app = angular.module('myApp', []);\napp.controller('myCtrl', function($scope) {\n    $scope.name = \"John Doe\";\n});\n</script>",
+    lang: 'html',
+    deps: ['angularjs']
+  }
+};
+/**
+log([
+    '测试'.spell(), // 返回 'CeShi'
+    '测试'.spell('up'), // 返回 'CESHI'
+    '测试'.spell('low'), // 返回 'ceshi'
+    '测试'.spell('first'), // 返回 'CS'
+    '测试'.spell('first', 'low'), // 返回 'cs'
+    '测试'.spell('array'), // 返回 ['Ce','Shi']
+    '测试'.spell('array', 'first', 'low'), // 返回 ['c','s']
+    '测试'.spell('tone'), // 返回 'CèShì'
+    '长大了'.spell('poly'), // 返回 '(Zhang|Chang)(Da|Dai)(Le|Liao)'
+    cnchar.spell('长大了', 'poly', 'tone')
+]);
+log(cnchar.spell('长大了', 'tone'));
+log([
+    '中华人民共和国'.stroke(),
+    '中华人民共和国'.stroke('array'),
+    cnchar.stroke('我爱中华人民共和国', 'array')
+]);
+log([
+    '一个'.stroke('order'),
+    '一个'.stroke('order', 'detail'),
+    '一个'.stroke('order', 'shape'),
+    cnchar.stroke('一个', 'order', 'name'),
+    cnchar.stroke('一个', 'order', 'count'),
+]);
+log([
+    cnchar.orderToWord(['横', '撇', '捺']),
+    cnchar.orderToWord(['横', '撇', '捺'], 'array'),
+    cnchar.orderToWord(['横', '撇', '捺'], 'start'),
+    cnchar.orderToWord(['横', '撇', '捺'], 'start', 'simple')
+]);
+log([
+    cnchar.spellToWord('shàng'),
+    cnchar.spellToWord('shàng', 'alltone'),
+    cnchar.spellToWord('shang4', 'alltone'),
+    cnchar.spellToWord('shang4', 'alltone', 'trad'),
+    cnchar.spellToWord('lv2', 'simple')
+]);
+log([
+    cnchar.strokeToWord(25),
+    cnchar.strokeToWord(25, 'simple'),
+    cnchar.strokeToWord(2, 'array')
+]);
+log([
+    cnchar.convert.simpleToTrad('一个人'),
+    '一个人'.convertSimpleToSpark(),
+    '壹個人'.convertTradToSimple()
+]);
+ */
---
+++ envs/lib.js
@@ -0,0 +1,159 @@
+"use strict";
+
+window.jsbox_libs = {
+  'jquery': {
+    'version': '3.4.1',
+    'url': 'https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js'
+  },
+  'alins': {
+    'version': '0.0.7',
+    'url': 'https://cdn.jsdelivr.net/npm/alins'
+  },
+  'alins-style': {
+    'version': '0.0.7',
+    'url': 'https://cdn.jsdelivr.net/npm/alins-style'
+  },
+  'vue': {
+    'version': '2.6.11',
+    'url': 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.min.js'
+  },
+  'vue3': {
+    'version': 'latest',
+    'url': 'https://cdn.jsdelivr.net/npm/vue'
+  },
+  'echarts': {
+    'version': 'latest',
+    'url': 'https://cdn.jsdelivr.net/npm/echarts'
+  },
+  'react': {
+    'version': '16.13.1',
+    'url': 'https://cdn.jsdelivr.net/npm/react@16.13.1/umd/react.production.min.js'
+  },
+  'react-dom': {
+    'version': '16.13.1',
+    'url': 'https://cdn.jsdelivr.net/npm/react-dom@16.13.1/umd/react-dom.production.min.js',
+    'deps': ['react']
+  },
+  'angularjs': {
+    'version': '1.7.9',
+    'url': 'https://cdn.bootcss.com/angular.js/1.7.9/angular.min.js'
+  },
+  'vuex': {
+    'version': '3.1.3',
+    'url': 'https://cdn.jsdelivr.net/npm/vuex@3.1.3/dist/vuex.min.js'
+  },
+  'redux': {
+    'version': '4.0.5',
+    'url': 'https://cdn.jsdelivr.net/npm/redux@4.0.5/lib/redux.min.js'
+  },
+  'loadsh': {
+    'version': '0.0.4',
+    'url': 'https://cdn.jsdelivr.net/npm/loadsh@0.0.4/lodash.min.js'
+  },
+  'virtual-dom': {
+    'version': '2.1.1',
+    'url': 'https://cdn.jsdelivr.net/npm/virtual-dom@2.1.1/dist/virtual-dom.js'
+  },
+  'node-html-parser': {
+    'version': '1.2.13',
+    'url': 'https://cdn.jsdelivr.net/npm/node-html-parser@1.2.13/dist/index.min.js'
+  },
+  'jest': {
+    'version': '25.1.0',
+    'url': 'https://cdn.jsdelivr.net/npm/jest@25.1.0/build/jest.min.js'
+  },
+  'mocha': {
+    'version': '7.1.1',
+    'url': 'https://cdn.jsdelivr.net/npm/mocha@7.1.1/lib/mocha.js'
+  },
+  'moment': {
+    'version': '2.24.0',
+    'url': 'https://cdn.jsdelivr.net/npm/moment@2.24.0/moment.min.js'
+  },
+  'dayjs': {
+    'version': '1.8.23',
+    'url': 'https://cdn.jsdelivr.net/npm/dayjs@1.8.23/dayjs.min.js'
+  },
+  'underscore': {
+    'version': '1.9.2',
+    'url': 'https://cdn.jsdelivr.net/npm/underscore@1.9.2/underscore.min.js'
+  },
+  'axios': {
+    'version': '0.19.2',
+    'url': 'https://cdn.jsdelivr.net/npm/axios@0.19.2/index.min.js'
+  },
+  'qrcode': {
+    'version': '1.4.4',
+    'url': 'https://cdn.jsdelivr.net/npm/qrcode@1.4.4/lib/index.min.js'
+  },
+  'backbone': {
+    'version': '1.4.0',
+    'url': 'https://cdn.jsdelivr.net/npm/backbone@1.4.0/backbone.min.js'
+  },
+  'js-xlsx': {
+    'version': '0.8.22',
+    'url': 'https://cdn.jsdelivr.net/npm/js-xlsx@0.8.22/dist/xlsx.min.js'
+  },
+  'recast': {
+    'version': '0.18.7',
+    'url': 'https://cdn.jsdelivr.net/npm/recast@0.18.7/main.min.js'
+  },
+  'cnchar': {
+    url: 'https://cdn.jsdelivr.net/npm/cnchar/cnchar.min.js'
+  },
+  'cnchar-poly': {
+    url: 'https://cdn.jsdelivr.net/npm/cnchar-poly/cnchar.poly.min.js',
+    deps: ['cnchar']
+  },
+  'cnchar-order': {
+    url: 'https://cdn.jsdelivr.net/npm/cnchar-order/cnchar.order.min.js',
+    deps: ['cnchar']
+  },
+  'cnchar-trad': {
+    url: 'https://cdn.jsdelivr.net/npm/cnchar-trad/cnchar.trad.min.js',
+    deps: ['cnchar']
+  },
+  'cnchar-draw': {
+    url: 'https://cdn.jsdelivr.net/npm/cnchar-draw/cnchar.draw.min.js',
+    deps: ['cnchar']
+  },
+  'cnchar-idiom': {
+    url: 'https://cdn.jsdelivr.net/npm/cnchar-idiom/cnchar.idiom.min.js',
+    deps: ['cnchar']
+  },
+  'cnchar-xhy': {
+    url: 'https://cdn.jsdelivr.net/npm/cnchar-xhy/cnchar.xhy.min.js',
+    deps: ['cnchar']
+  },
+  'cnchar-radical': {
+    url: 'https://cdn.jsdelivr.net/npm/cnchar-radical/cnchar.radical.min.js',
+    deps: ['cnchar']
+  },
+  'cnchar-words': {
+    url: 'https://cdn.jsdelivr.net/npm/cnchar-words/cnchar.words.min.js',
+    deps: ['cnchar']
+  },
+  'cnchar-explain': {
+    url: 'https://cdn.jsdelivr.net/npm/cnchar-explain/cnchar.explain.min.js',
+    deps: ['cnchar']
+  },
+  'cnchar-voice': {
+    url: 'https://cdn.jsdelivr.net/npm/cnchar-voice/cnchar.voice.min.js',
+    deps: ['cnchar']
+  },
+  'cnchar-all': {
+    url: 'https://cdn.jsdelivr.net/npm/cnchar-all/cnchar.all.min.js'
+  },
+  'easy-icon': {
+    'url': 'https://cdn.jsdelivr.net/gh/theajack/easy-icon/dist/easy-icon.min.css',
+    'type': 'style'
+  },
+  'element-ui': {
+    'url': 'https://cdn.jsdelivr.net/npm/element-ui@2.13.0/lib/index.js',
+    'deps': ['vue', 'element-ui-style']
+  },
+  'element-ui-style': {
+    'url': 'https://cdn.jsdelivr.net/npm/element-ui@2.13.0/lib/theme-chalk/index.css',
+    'type': 'style'
+  }
+};
